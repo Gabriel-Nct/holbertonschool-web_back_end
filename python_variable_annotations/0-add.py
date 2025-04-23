f542a8cbb0cf3a1d951e2e7eb4377c3c1cd9d35d
Gabriel-Nct <bescondgabriel@gmail.com>
2025-04-23 15:23:59 +0200

    add docstring before method

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+"""
+This module provides a function to add two numbers.
+It includes type annotations for better code clarity and
+type checking.
+"""
 def add(a: float, b: float) -> float:
     """Return the sum of a and b."""
     return a + b
