a384f04088f045e19c10f0ecdbe5ffa796bba0eb
Gabriel-Nct <bescondgabriel@gmail.com>
2025-04-23 16:52:48 +0200

    add task5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to sum all the elements of a lists.
+"""
+
+
+def sum_list(input_list: float) -> float:
+    """
+    Function to sum all the elements of a list
+    Args:
+        input_list: list of floats
+    Returns:
+        The sum of all the elements in the list
+    """
+    return sum(input_list)
