047db7c4135915c3344e528274d032113b3782e4
Gabriel-Nct <bescondgabriel@gmail.com>
2025-04-23 15:24:53 +0200

    fix pycodestyle

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -4,6 +4,8 @@ This module provides a function to add two numbers.
 It includes type annotations for better code clarity and
 type checking.
 """
+
+
 def add(a: float, b: float) -> float:
     """Return the sum of a and b."""
     return a + b
