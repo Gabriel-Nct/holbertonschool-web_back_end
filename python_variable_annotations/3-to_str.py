fc018ba238f22b4da67cbcd68de6090804ca75a0
Gabriel-Nct <bescondgabriel@gmail.com>
2025-04-23 16:11:38 +0200

    add type-annotated function to_str that takes a float n as argument and returns the string representation of the float

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to add two numbers.
+The function takes two float arguments and returns their sum as a float.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Convert a float to a string.
+    """
+    return str(n)
