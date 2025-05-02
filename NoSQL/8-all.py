b22512da1c08a67ccf6d8c494db01afe716a5ac6
Gabriel-Nct <bescondgabriel@gmail.com>
2025-05-02 14:00:12 +0200

    add task 8

---
+++ NoSQL/8-all.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Module that defines a function to list
+all documents in a MongoDB collection.
+"""
+
+
+def list_all(mongo_collection):
+    """Lists all documents in a collection"""
+    return list(mongo_collection.find()) if mongo_collection else []
